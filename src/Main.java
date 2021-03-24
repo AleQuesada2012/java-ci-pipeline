0491b5034737b35bbdc69391781ff0ec3b6c9db7
AleQuesada2012 <AleQuesada2012@users.noreply.github.com>
2021-03-24 09:34:15 -0600

    Add calculator methods and main class for execution

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("to test the calculator methods, these must be added to the main execution block");
+    }
+}
---
+++ src/SimpleCalculator.java
@@ -0,0 +1,36 @@
+public class SimpleCalculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int substract(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
+
+    // float methods
+
+    public float add(float a, float b) {
+        return a+b;
+    }
+
+    public float substract(float a, float b) {
+        return a-b;
+    }
+
+    public float multiply(float a, float b) {
+        return a*b;
+    }
+
+    public float divide(float a, float b) {
+        return a/b;
+    }
+}
